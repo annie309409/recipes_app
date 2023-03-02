2129551cb049332a82d42142910227d932065899
annie309409 <pjatopaz1@naver.com>
2023-03-02 11:00:24 +0900

    초기세팅

---
+++ readme.md
@@ -0,0 +1,7 @@
+# recipe-app
+## learning react 2dn ed.
++ npm init
++ webpack
++ babel
++ react@16.14
++ 
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js", //진입(시작) 파일
+    output: {
+        path: path.join(__dirname, "dist"), //번들파일 저장위치
+        filename: "app.bundle.js" //지정하지 않으면 main.js
+    },
+    module: { //각 파일에 대한 세부적인 번들링 작업 정의
+        rules: [{ test: /\.js$/, //확장자가 .js로 끝나는 파일에 대한 규칙정의
+            exclude: /node_modules/, loader: "babel-loader" }]
+    },
+    devServer: {// npm start명령어로 서버 구동 시 필요한 관련 내용 설정
+        static: path.join(__dirname, 'dist'),
+        open: true, // 자동으로 브라우저 실행
+        hot: true, // 수정사항 발생시 브라우저에 변경사항 즉시 반영
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // 빌드시 dist안에 index.html을 바로 생성하는 플러그인
+            template: path.join(__dirname, "public", "index.html"),
+        }),
+    ],
+};
